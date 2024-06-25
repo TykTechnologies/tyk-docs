fb726611743311b58207ab2bc964d4e822a9b965
Simon Pears <simon@tyk.io>
2024-06-25 12:11:40 +0100

    update versions so Latest after release name

--- scripts/page_available_since.py
+++ scripts/page_available_since.py
@@ -4,12 +4,12 @@ import json
 versions = [
     {
         "path": "/docs/",
-        "name": "Latest - 5.4",
+        "name": "5.4 - Latest",
         "branch": "release-5.4"
     },
     {
         "path": "/docs/5.3/",
-        "name": "5.3",
+        "name": "5.3 LTS",
         "branch": "release-5.3"
     },
     {
