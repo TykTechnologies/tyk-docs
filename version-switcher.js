ccdd435aa41eabd1734cc789330c88a3d458f4b6
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2025-06-10 18:01:13 +0300

    add release-5.8

--- version-switcher.js
+++ version-switcher.js
@@ -7,7 +7,7 @@ const PATH_PREFIX = '/docsv2';  // Change to '/docs' when ready for production
 function hijackVersionDropdown() {
     // Version mapping from docs.json
     const versionConfig = {
-        '5.8-Latest': 'https://tyk.io/docsv2',
+        'Nightly': 'https://tyk.io/docsv2/nightly',
         '5.7': 'https://tyk.io/docsv2/5.7',
         '5.6': 'https://tyk.io/docs/5.6',
         '5.5': 'https://tyk.io/docs/5.5'
@@ -138,4 +138,3 @@ setTimeout(hijackVersionDropdown, 1000);
 setTimeout(hijackVersionDropdown, 3000);
 
 console.log('Version hijack script loaded and observing!');
-
